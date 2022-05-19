c7ab3b14c725e255406f91759b66fea9fdd5b3ce
RafiqGrocott <99296457+RafiqGrocott@users.noreply.github.com>
2022-05-20 09:54:50 +1200

    Add files via upload

---
+++ 03_HLG_Instructions.py
@@ -0,0 +1,10 @@
+# Show instrustions
+def instructions():
+        print("*****Instrustions*****")
+        print()
+        print("Rules of the game...")
+        print()
+        print("Type in the number of rounds you would like to play OR,"
+                "press <enter> to play infinite mode.")
+        print()
+        print("Next, you will guess your number (between your high number and low number). The computer will then tell you if your guess was too high, or too low")
